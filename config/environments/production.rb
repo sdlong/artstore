8a4d59480e4845689c28a168f3eadd1d074c564b
misuchiru <perryhaha@gmail.com>
2015-11-30 20:58:57 +0800

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
+  config.action_mailer.default_url_options = { host: 'https://whispering-mesa-8399.herokuapp.com/' }
 end
