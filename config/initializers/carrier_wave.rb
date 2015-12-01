aa3684aca5bb17c5d22a1a8965590fbab1311cd2
perry <perry@perrydeiMac.local>
2015-12-02 00:35:44 +0800

    設定 AWS figaro

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'xxx',      # 你的 key
+      aws_access_key_id:     ENV["aws_access_key_id"],      # 你的 key
 
-      aws_secret_access_key: 'yyy',      # 你的 secret key
+      aws_secret_access_key: ENV["aws_secret_access_key"],      # 你的 secret key
 
       region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
 
