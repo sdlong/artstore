9bc61f7cd99c3dcc60c37bb871648e2c52b337cb
misuchiru <perryhaha@gmail.com>
2015-12-02 17:44:52 +0800

    修改AWS圖片上傳region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
 
       aws_secret_access_key: ENV["aws_secret_access_key"],      # 你的 secret key
 
-      region:                'Tokyo' # 你的 S3 bucket 的 Region 位置
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
 
     }
     config.fog_directory  = 'perry1001' # 你設定的 bucket name
