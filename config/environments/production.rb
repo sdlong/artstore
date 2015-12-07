65e837e958231040c60390dc71a1257af570bcee
scarlett2003 <jovilee@vialleytekiMacBook-Pro.local>
2015-12-07 18:12:10 +0800

    完成 production 寄信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     user_name: ENV["mailgun_user"],
     password: ENV["mailgun_secret"],
     domain: "sandbox51776498dafb4c67acdebac066da1423.mailgun.org",
-    authentication: plain,
+    authentication: :plain,
   }
 
 end
