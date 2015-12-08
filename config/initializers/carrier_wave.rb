ae59bc51d535b14e7072d085cbc310eb5d9a328a
6alley <jovilee09@gmail.com>
2015-12-08 14:42:04 +0800

    i

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
 		config.fog_credentials = {
 			provider: 'AWS',
 			aws_access_key_id: ENV['aws_access_key_id'],
-			aws_secret_access_keys: ENV['aws_secret_access_keys'],
+			aws_secret_access_key: ENV['aws_secret_access_key'],
 			region: 'us-west-1'
 		}
 		config.fog_directory = 'jovi-artstore-v3'
