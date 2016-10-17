063f273d83bc45666558f77cfe21c80c69df6ef2
sdlong <sdlong.jeng@gmail.com>
2016-10-17 09:08:31 +0800

    使用 subject 來讓測試程式更簡潔

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { User.create(email: email,
+                               password: password,
+                               password_confirmation: password_confirmation) }
+
+  # 設定 subject 之後, expect(user) 即可簡化成 is_expected.to (將 subject 當成預設用來做測試判斷的物件)
+
+  let(:email) { "user@test.com" }
+  let(:password) { "12345678" }
+  let(:password_confirmation) { "12345678" }
+
   it "第一個測試" do            # 每一個 it 都代表一個測試的項目
-    expect(User).to be_present # expect 是用來對測試下預期的判斷, 執行結果會顯示在 rspec 運作訊息裡
+    is_expected.to be_present # expect 是用來對測試下預期的判斷, 執行結果會顯示在 rspec 運作訊息裡
   end
 
   it "測試 user 建立成功" do
-    user = User.create(email: "user@test.com",
-                       password: "12345678",
-                       password_confirmation: "12345678")
-    expect(user).to be_valid   # 等同於 user.valid? == true  , 代表 ActiveModel::Validations pass 而且沒有任何 errors
+    is_expected.to be_valid   # 等同於 user.valid? == true  , 代表 ActiveModel::Validations pass 而且沒有任何 errors
   end
 
   it "測試 user 建立失敗" do
