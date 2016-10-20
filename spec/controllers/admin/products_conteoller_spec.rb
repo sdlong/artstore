297148c4b33adb7b21856e6ec06d874656a97966
sdlong <sdlong.jeng@gmail.com.tw>
2016-10-20 16:44:24 +0800

    完成 index 的測試

--- spec/controllers/admin/products_conteoller_spec.rb
+++ spec/controllers/admin/products_conteoller_spec.rb
@@ -5,7 +5,37 @@ RSpec.describe Admin::ProductsController, type: :controller do
   subject(:normal_user){ User.create(email: "user@test.com", password: "12345678", password_confirmation: "12345678") }
   subject(:product)    { Product.create(title: "macbook", price: "60000", quantity: "5", description: "蘋果電腦")}
 
-  describe "GET index"
+  describe "GET index" do
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { get :index }
+
+      it { expect(response).to be_success }
+      it { expect(response.header['Content-Type']).to include "text/html" }
+    end
+
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { get :index }
+
+      it { expect(response).to redirect_to root_path }
+    end
+
+    context "login admin_user with json" do
+      before { sign_in :user, admin_user }
+      before { get :index, format: :json }
+
+      it { expect(response).to be_success }
+      it { expect(response.header['Content-Type']).to include "application/json" }
+    end
+
+    context "login normal_user with json" do
+      before { sign_in :user, normal_user }
+      before { get :index, format: :json }
+
+      it { expect(response).to redirect_to root_path }
+    end
+  end
 
   describe "GET new"
 
